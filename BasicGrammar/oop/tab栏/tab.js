1f752c5042eae89f0f8612092e9b5a7ad412b2ef
Vinylonprophet <915390118@qq.com>
2022-09-13 00:42:46 +0800

    tab's add module has finished

--- BasicGrammar/oop/tab栏/tab.js
+++ BasicGrammar/oop/tab栏/tab.js
@@ -2,7 +2,7 @@
  * @Author: Vinylonprophet 915390118@qq.com
  * @Date: 2022-09-12 22:12:34
  * @LastEditors: Vinylonprophet 915390118@qq.com
- * @LastEditTime: 2022-09-12 23:01:49
+ * @LastEditTime: 2022-09-13 00:34:20
  * @FilePath: \oop\tab栏\tab.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -14,14 +14,26 @@ class Tab{
         this.main = document.querySelector(id);
         this.lis = this.main.querySelectorAll('li');
         this.sections = this.main.querySelectorAll('section');
+        this.add = this.main.querySelector('.tabadd');
+        // li的父元素ul
+        this.ul = this.main.querySelector('.fisrstnav ul:first-child');
+        // section的父元素
+        this.fsection = this.main.querySelector('.tabscon');
         this.init();
     }
     init(){
+        this.updateNode();
         // init 初始化操作让相关的元素绑定事件
+        this.add.onclick = this.addTab;
         for(var i = 0; i < this.lis.length; i++){
             this.lis[i].index = i;
             this.lis[i].onclick = this.toggleTab;
         }
+    }
+    // 获取所有的li和section
+    updateNode(){
+        this.lis = this.main.querySelectorAll('li');
+        this.sections = this.main.querySelectorAll('section');
     }
     // 1.切换功能
     toggleTab(){
@@ -37,7 +49,15 @@ class Tab{
     }
     // 2.添加功能
     addTab(){
-
+        that.clearClass();
+        // 创建li元素和section元素
+        var random = Math.random();
+        var li = '<li class="liactive"><span>新选项卡</span><span class="iconfont icon-guanbi"></span></li>';
+        var section = '<section class="conactive">测试 '+ random +'</section>';
+        // 把两个元素追加到对应的父元素里面
+        that.ul.insertAdjacentHTML('beforeend', li);
+        that.fsection.insertAdjacentHTML('beforeend', section);
+        that.init();
     }
     // 3.删除功能
     removeTab(){
