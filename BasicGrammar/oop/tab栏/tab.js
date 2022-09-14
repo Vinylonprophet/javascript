5233f4e38218fb1dd36fee331a3153bef81851ed
Vinylonprophet <915390118@qq.com>
2022-09-14 23:29:16 +0800

    tab has finished

--- BasicGrammar/oop/tab栏/tab.js
+++ BasicGrammar/oop/tab栏/tab.js
@@ -2,18 +2,16 @@
  * @Author: Vinylonprophet 915390118@qq.com
  * @Date: 2022-09-12 22:12:34
  * @LastEditors: Vinylonprophet 915390118@qq.com
- * @LastEditTime: 2022-09-13 00:34:20
+ * @LastEditTime: 2022-09-14 23:28:10
  * @FilePath: \oop\tab栏\tab.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 var that;
-class Tab{
-    constructor(id){
+class Tab {
+    constructor(id) {
         // 获取元素
         that = this;
         this.main = document.querySelector(id);
-        this.lis = this.main.querySelectorAll('li');
-        this.sections = this.main.querySelectorAll('section');
         this.add = this.main.querySelector('.tabadd');
         // li的父元素ul
         this.ul = this.main.querySelector('.fisrstnav ul:first-child');
@@ -21,51 +19,79 @@ class Tab{
         this.fsection = this.main.querySelector('.tabscon');
         this.init();
     }
-    init(){
+    init() {
         this.updateNode();
         // init 初始化操作让相关的元素绑定事件
         this.add.onclick = this.addTab;
-        for(var i = 0; i < this.lis.length; i++){
+        for (var i = 0; i < this.lis.length; i++) {
             this.lis[i].index = i;
             this.lis[i].onclick = this.toggleTab;
+            this.remove[i].onclick = this.removeTab;
+            this.spans[i].ondblclick = this.editTab;
+            this.sections[i].ondblclick = this.editTab;
         }
     }
     // 获取所有的li和section
-    updateNode(){
+    updateNode() {
         this.lis = this.main.querySelectorAll('li');
         this.sections = this.main.querySelectorAll('section');
+        this.remove = this.main.querySelectorAll('.icon-guanbi');
+        this.spans = this.main.querySelectorAll('.fisrstnav li span:first-child');
     }
     // 1.切换功能
-    toggleTab(){
+    toggleTab() {
         that.clearClass();
         this.className = 'liactive';
         that.sections[this.index].className = 'conactive';
     }
-    clearClass(){
-        for(var i = 0; i < this.lis.length; i++){
+    clearClass() {
+        for (var i = 0; i < this.lis.length; i++) {
             this.lis[i].className = '';
             this.sections[i].className = '';
         }
     }
     // 2.添加功能
-    addTab(){
+    addTab() {
         that.clearClass();
         // 创建li元素和section元素
         var random = Math.random();
         var li = '<li class="liactive"><span>新选项卡</span><span class="iconfont icon-guanbi"></span></li>';
-        var section = '<section class="conactive">测试 '+ random +'</section>';
+        var section = '<section class="conactive">测试 ' + random + '</section>';
         // 把两个元素追加到对应的父元素里面
         that.ul.insertAdjacentHTML('beforeend', li);
         that.fsection.insertAdjacentHTML('beforeend', section);
         that.init();
     }
     // 3.删除功能
-    removeTab(){
-
+    removeTab(e) {
+        e.stopPropagation();    //  阻止冒泡 防止触发li的点击事件
+        var index = this.parentNode.index;
+        // 根据索引号删除对应li和section    remove直接删除
+        that.lis[index].remove();
+        that.sections[index].remove();
+        that.init();
+        if (document.querySelector('.liactive')) return;
+        index--;
+        // 前面为真调用,为假不调用
+        that.lis[index] && that.lis[index].click();
     }
     // 4.修改功能
-    editTab(){
-        
+    editTab() {
+        var str = this.innerHTML;
+        // 双击禁止选定文字
+        window.getSelection ? window.getSelection().removeAllRanges() : document.getSelection.empty();
+        this.innerHTML = '<input type="text" />';
+        var input = this.children[0];
+        input.value = str;
+        input.select();
+        input.onblur = function () {
+            this.parentNode.innerHTML = this.value;
+        }
+        input.onkeyup = function(e){
+            if(e.keyCode === 13){
+                this.blur();
+            }
+        }
     }
 }
 new Tab('#tab')
